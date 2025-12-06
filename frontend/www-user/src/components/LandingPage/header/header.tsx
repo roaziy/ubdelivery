bd942654bb864932b50e9fe87e339da03016589a
roaziy <erkhemtur389@gmail.com>
2025-12-06 23:24:33 +0800

    navbar fix absolute to fixed

--- frontend/www-user/src/components/LandingPage/header/header.tsx
+++ frontend/www-user/src/components/LandingPage/header/header.tsx
@@ -3,7 +3,7 @@ import HeaderMobile from "./headerMobile";
 
 export default function Header() {
     return (
-        <header className="absolute top-0 left-0 w-full bg-white shadow-lg shadow-gray-200/50">
+        <header className="fixed top-0 left-0 w-full bg-white shadow-lg shadow-gray-200/50">
             <HeaderDesktop />
             <HeaderMobile />
         </header>
