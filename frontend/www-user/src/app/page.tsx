7b2893e328150fd058c9ecffc5c68a187d95b7a1
roaziy <erkhemtur389@gmail.com>
2025-12-07 14:13:01 +0800

    landing page

--- frontend/www-user/src/app/page.tsx
+++ frontend/www-user/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { IoLocationSharp } from "react-icons/io5";
 
 import Header from "../components/LandingPage/header/header";
 import Footer from "../components/LandingPage/footer/footer";
@@ -12,12 +13,24 @@ export default function Home() {
             <h1 className="text-[30px] md:text-[40px] mt-24 md:mt-0  font-bold mb-6 leading-[115%] md:pl-10">
             Монголын <span className="text-mainGreen" >хамгийн хурдан</span> хоол хүргэлтийн платформ
             </h1>
-          <p className="text-lg mb-8 md:pl-10 md:mt-16">
+          <p className="text-sm md:text-lg mb-8 md:pl-10 md:mt-16">
             Хүргэлтийн үйлчилгээг шинэ түвшинд аваачихад бидэнтэй нэгдээрэй.
           </p>
-          {/* <button className="bg-mainGreen text-white px-6 py-3 rounded-full hover:bg-green-600 transition-colors">
-            Апп татах
-          </button> */}
+          <div className="md:pl-10 mt-6">
+            <div className="flex items-center bg-white border-1 border-gray-300 rounded-full py-[2px] overflow-hidden max-w-md">
+              <div className="flex items-center pl-4 pr-2">
+                <IoLocationSharp className="text-[#848484]" size={20} />
+              </div>
+              <input 
+                type="text" 
+                placeholder="Хүргэлт хийх хаяг" 
+                className="flex-1 py-3 px-2 outline-none text-sm"
+              />
+              <button className="bg-mainGreen text-white px-6 py-2 mr-2 rounded-full hover:bg-green-600 transition-colors font-medium text-sm">
+                Хайх
+              </button>
+            </div>
+          </div>
         </div>
         <div className="flex h-full items-center justify-center relative ">
           <div className="absolute w-[200px] h-[200px] md:w-[300px] md:h-[300px] bg-[#4bff45] rounded-full blur-3xl opacity-10 ml-10 md:ml-20"></div>
